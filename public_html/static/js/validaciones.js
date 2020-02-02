27b8047139a9c6739b375f44bea79fe30c179095
kary <kairy159@gmail.com>
2020-02-01 22:42:19 -0600

    Intento de validaciones

--- public_html/static/js/validaciones.js
+++ public_html/static/js/validaciones.js
@@ -1,15 +1,32 @@
-function validarContrasenia()
-{ 
 
-var contrasenia = document.getElementById("contrasenia");
-var contrasenia2 = document.getElementById("contrasenia2");    
- 
-        
-
-}//Termina validar registo
+function validarPass(){
+    var contrasenia = document.getElementById("contrasenia");
+    var numero="0123456789";
+    var minus="abcdefghyjklmnñopqrstuvwxyz";
+    var mayusculas="ABCDEFGHYJKLMNÑOPQRSTUVWXYZ";
+   // var expre = (^[A-Za-z\d$@$!%*?&]+$);
+    if(contrasenia.length < 10){		
+        alert("Debe contener un minimo de 10 caracateres");
+    for(i=0; i<contrasenia.length; i++){
+      if (mayusculas.indexOf(contrasenia.charAt(i),0)!=-1){
+         return 1;
+      }
+   }
+    }
+}//Termina validar  contrasenia
            
 
+function validarContraseniaIgual()
+{ 
+    var contrasenia = document.getElementById("contrasenia");
+    var contrasenia2 = document.getElementById("contrasenia2");    
+    
+    if (pcontrasenia != contrasenia2) {
+      alert("Las passwords no coinciden");
+      //return false;
+    } 
 
+}//Termina validar registo
             
             
             

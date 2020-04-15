898a6c890168fd5fbe18c6bf0c344edef5c2c4cd
aki801 <kairy159@gmail.com>
2020-04-14 23:40:04 -0500

    Se cambio la forma de mostrar los mensajes por onkeyup

--- public_html/static/js/validaciones.js
+++ public_html/static/js/validaciones.js
@@ -1,4 +1,4 @@
-function validarPass() {
+/*function validarPass() {
     var contrasenia = document.getElementById("contrasenia").value;
     var expReg = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})");
     var comparacion = expReg.test(contrasenia);
@@ -22,5 +22,32 @@ function validarPass() {
 }
 //Termina validar  contrasenia*/
            
+function validarPass() {
+    var contrasenia = document.getElementById("contrasenia").value;
+    var expReg = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})");
+    var comparacion = expReg.test(contrasenia);
+    var error1 = document.getElementById("msjPassError1");
+    
+    if(comparacion == false){
+        error1.style.display= "block";
+    }
+    else if(comparacion == true){
+        error1.style.display= "none";
+    }  
+}
 
+//document.getElementById("contrasenia2").onkeyup = function() {validarPass2()};
 
+function validarPass2() {
+    var contrasenia = document.getElementById("contrasenia").value;
+    var contrasenia2 = document.getElementById("contrasenia2").value;
+    var error2 = document.getElementById("msjPassError2");
+    
+    if (contrasenia != contrasenia2) {
+        error2.style.display="block";
+    }
+    else if(contrasenia == contrasenia2){
+        error2.style.display="none";
+    }
+    
+}
